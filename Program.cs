3dffd5e087b9cabd3b5424fdd01fadf25fb3b298
oleja_kosmolet <olegermolaev45@gmail.com>
2023-11-26 20:35:08 +0300

    Fixed Program.cs

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = {"Hello", "2", "world", ":-)"};
+            NewArray(array);
+        }
+
+        static string[] NewArray (string[] array)
+        {
+            int value = 3;
+            int count = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i].Length <= value)
+                    count++;
+            }
+
+            string[] resultArray = new string[count];
+
+            int j = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i].Length <= value)
+                {
+                    resultArray[j] = array[i];
+                    j++;
+                }
+            }
+
+            /*for (int i = 0; i < resultArray.Length; i++)
+            {
+                Console.Write(resultArray[i] + " ");
+            }*/
+
+            return resultArray;
+        }
+    }
+}
